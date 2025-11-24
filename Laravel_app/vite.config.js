453da392c1e6487bbe53aa9b52f289d8b23a797e
FitoPero <fito.pero@gmail.com>
2025-11-24 13:45:25 -0300

    first commit

---
+++ Laravel_app/vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig, loadEnv } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        server: {
+            host: '0.0.0.0',
+            port: env.VITE_HMR_PORT,
+            strictPort: true,
+            watch: {
+                usePolling:'true',
+            },
+            hmr: {
+                host: env.VITE_HMR_HOST,
+                port: env.VITE_HMR_PORT,
+                protocol: env.VITE_HMR_PROTOCOL,
+            }
+        },
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+            tailwindcss(),
+        ],
+    };
+});
